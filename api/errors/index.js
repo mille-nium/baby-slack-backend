5cbc99478fda8d8f8ae7d8f09411ebe9bcfa84b6
mille-nium <dianaboloten.db@gmail.com>
2019-10-06 16:06:50 +0300

    fix: add username validation

--- api/errors/index.js
+++ api/errors/index.js
@@ -1,9 +1,8 @@
 'use strict';
 
 const user = {
-  ERR_WEAK_PASSWORD: 600,
+  ERR_INVALID_CREDENTIALS: 600,
   ERR_USERNAME_ALREADY_EXISTS: 601,
-  ERR_INVALID_CREDENTIALS: 602,
 };
 
 module.exports = {
