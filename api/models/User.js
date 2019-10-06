4ebddfe74ac405f458b4a90985fb1df5a3881a94
mille-nium <dianaboloten.db@gmail.com>
2019-10-06 16:06:50 +0300

    fix: add username validation

--- api/models/User.js
+++ api/models/User.js
@@ -11,26 +11,24 @@ const userSchema = new Schema({
     type: String,
     unique: 'User already exists',
     required: 'Property "username" is required',
+    validate: {
+      validator: v => /^[\w_.-]{5,32}$/.test(v),
+      message: props => `${props.value} is not a valid username`,
+    },
   },
   password: {
     type: String,
     required: 'Property "password" is required',
+    validate: {
+      validator: v => owasp.test(v).strong,
+    },
   },
 });
 
 userSchema.pre('save', async function(next) {
   try {
-    const testPassword = owasp.test(this.password);
-
-    if (!testPassword.strong) {
-      const err = new Error(errors.ERR_WEAK_PASSWORD);
-      err.status = 422;
-      err.details = testPassword.errors;
-      next(err);
-    } else {
-      this.password = await argon2.hash(this.password);
-      next();
-    }
+    this.password = await argon2.hash(this.password);
+    next();
   } catch (err) {
     next(err);
   }
@@ -38,10 +36,27 @@ userSchema.pre('save', async function(next) {
 
 userSchema.post('save', (err, doc, next) => {
   if (err.name === 'MongoError' && err.code === 11000) {
-    const err = new Error(errors.ERR_USERNAME_ALREADY_EXISTS);
-    err.status = 422;
-    err.details = ['This username already in use'];
-    next(err);
+    const error = new Error(errors.ERR_USERNAME_ALREADY_EXISTS);
+    error.status = 422;
+    error.details = ['This username already in use'];
+    next(error);
+  } else if (err.name === 'ValidationError') {
+    const error = new Error(errors.ERR_INVALID_CREDENTIALS);
+    error.status = 422;
+    error.details = [];
+    const errorValues = Object.keys(err.errors);
+
+    if (errorValues.includes('password')) {
+      const { errors } = owasp.test(doc.password);
+      error.details = error.details.concat(errors);
+    }
+
+    if (errorValues.includes('username')) {
+      const { message } = err.errors.username;
+      error.details.push(message);
+    }
+
+    next(error);
   } else {
     next();
   }
