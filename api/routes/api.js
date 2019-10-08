d96ec4ca173ead4bea78af37cd26759d5a4192e8
mille-nium <dianaboloten.db@gmail.com>
2019-10-08 19:44:04 +0300

    feat: implement get chat messages

--- api/routes/api.js
+++ api/routes/api.js
@@ -28,6 +28,16 @@ router.get('/chats/:chatId', async (ctx, next) => {
   await authenticate(ctx, next);
 });
 
+router.get('/:chatId/messages/:ms', async (ctx, next) => {
+  const { chatId, ms } = ctx.params;
+  const authenticate = await passport.authenticate(
+    'jwt',
+    { session: false },
+    ChatController.getMessages(ctx, chatId, ms)
+  );
+  await authenticate(ctx, next);
+});
+
 router.get('/:chatId/uninvited-users', async (ctx, next) => {
   const { chatId } = ctx.params;
   const authenticate = await passport.authenticate(
