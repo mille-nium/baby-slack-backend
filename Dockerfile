af71b5140b1bc1cfe292c59370007a2f20fa8b16
mille-nium <dianaboloten.db@gmail.com>
2019-12-15 20:30:13 +0200

    feat: add containerization

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,8 @@ FROM node:latest
 WORKDIR /home/app
 COPY . .
 
+RUN npm install
+
 CMD ["node", "app.js"]
+
+EXPOSE 3003
--- src/errors/index.js
+++ src/errors/index.js
@@ -3,6 +3,7 @@
 const user = {
   ERR_INVALID_CREDENTIALS: 600,
   ERR_USERNAME_ALREADY_EXISTS: 601,
+  ERR_NO_SUCH_USER: 603,
 };
 
 const errors = {
